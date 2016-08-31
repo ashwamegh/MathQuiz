f77877b32b77721a1df6880be07d703ac70e34a4
shashank7200 <shashank7200@gmail.com>
2016-08-31 23:36:38 +0530

    Few changes introducing intents

--- app/src/main/java/com/example/ashwamegh/mathquiz/QuizActivity.java
+++ app/src/main/java/com/example/ashwamegh/mathquiz/QuizActivity.java
@@ -14,6 +14,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
+    private Button mCheatButton;
 
     private QuestionBank[] mQuestionBank = new QuestionBank[]{
             new QuestionBank(R.string.question_1, true),
@@ -46,7 +47,7 @@ public class QuizActivity extends AppCompatActivity {
 
 
 
-        //mTrueButton= (Button) findViewById(R.id.true_button);
+        mTrueButton= (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
