55f8d98e5d39460d9f5d347a9243d4586d6d5f6b
shashank7200 <shashank7200@gmail.com>
2016-09-01 00:57:33 +0530

    Few changes introducing intents

--- app/src/main/java/com/example/ashwamegh/mathquiz/CheatActivity.java
+++ app/src/main/java/com/example/ashwamegh/mathquiz/CheatActivity.java
@@ -1,13 +1,26 @@
 package com.example.ashwamegh.mathquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 public class CheatActivity extends AppCompatActivity {
 
-
     public static final String TAG="CheatActivity";
+    public static final String Cheat_Index= "CheatIndex";
+
+
+    public static Intent newIntent(Context context, int i){
+        Intent intent= new Intent(context, CheatActivity.class);
+        intent.putExtra(Cheat_Index,i);
+        
+        return intent;
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
